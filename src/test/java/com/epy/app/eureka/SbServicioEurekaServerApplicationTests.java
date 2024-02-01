f3a15a03815a27b379227493a7ad0e3a1a2dce99
Erick Pariona Yauricasa <erick.pariona@hotmail.com>
2024-02-01 01:09:57 -0500

    CONFIG_INI

---
+++ src/test/java/com/epy/app/eureka/SbServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epy.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
